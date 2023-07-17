f60f34e52a9392a8558bf47610854fceb98ef544
Stephanie Cruz <stephaniecruzcastelli@gmail.com>
2023-07-17 12:22:43 -0400

    Se agrega header fixed al hacer scroll

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,32 +9,32 @@ function Header() {
     const [background, setBackground] = useState(location.pathname === '/' ? 'bg-transparent' : 'bg-white');
 
 
-    // useEffect(() => {
-    //     if (location.pathname !== '/') return
-    //       const handleScroll = () => {
-    //         const sectionElement = document.getElementById('banner');
-    //         const rect = sectionElement.getBoundingClientRect();
-    //         const isAtBottom = Math.abs(rect.y) >= rect.height
-    //         if (isAtBottom) {
-    //           setBackground('bg-white')
-    //           // Perform any actions you want when the scroll reaches the target section
-    //         }
-    //         else { setBackground('bg-transparent')}
-    //       };
+    useEffect(() => {
+        if (location.pathname !== '/') return
+          const handleScroll = () => {
+            const sectionElement = document.getElementById('banner');
+            const rect = sectionElement.getBoundingClientRect();
+            const isAtBottom = Math.abs(rect.y) >= rect.height
+            if (isAtBottom) {
+              setBackground('bg-white')
+              // Perform any actions you want when the scroll reaches the target section
+            }
+            else { setBackground('bg-transparent')}
+          };
     
-    //       window.addEventListener('scroll', handleScroll);
+          window.addEventListener('scroll', handleScroll);
       
-    //       return () => {
-    //         window.addEventListener('scroll', handleScroll);
-    //       }
+          return () => {
+            window.addEventListener('scroll', handleScroll);
+          }
     
-    // }, [location.pathname]);
+    }, [location.pathname]);
 
   return (
     <header className={`header flex justify-content-between ${background !== 'bg-transparent' && 'header__white'}`}>
         <div className="container flex justify-content-between align-items-center">
             <img className='logo' src={logo} alt="Beauty Americas" />
-            <Svg classes='menu' name='burger-menu' fill='#fff'/>
+            <Svg classes='menu' name='burger-menu' fill={background === 'bg-transparent' ? '#fff' : '#606060'}/>
         </div>
     </header>
   )
